02e8ba2b975e64fb15af65881d11843b48eefe6a
romulopalacios <e1314685197@live.uleam.edu.ec>
2025-07-30 21:23:51 -0500

    Prueba pipeline Jenkins automático

--- mi-proyecto-ci/index.js
+++ mi-proyecto-ci/index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
-    res.send('Hola mundo desde Jenkins Pipeline!');
+    res.send('¡Hola mundo desde Jenkins Pipeline - Pipeline Test v2.0! 🚀');
+});
+
+app.get('/health', (_, res) => {
+    res.json({ status: 'OK', timestamp: new Date().toISOString() });
 });
 
 app.listen(PORT, () => {
